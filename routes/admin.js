4c98d9739f6b49b916fe32b7e177327956e3fee6
rustamin <rustamjr_11@yahoo.com>
2016-02-07 22:51:36 +0700

    udah bisa ganti nama

--- routes/admin.js
+++ routes/admin.js
@@ -2,7 +2,41 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 
+var path = require('path');
 var multer = require('multer');
+
+// var storage = multer.diskStorage({
+// 	destination: function(req, file, cb) {
+// 		cb(null, '/tmp/my-uploads');
+// 	},
+// 	filename: function(req, file, cb) {
+// 		cb(null, file.fieldname + '-' + Date.now());
+// 	}
+// });
+
+// ORIGINAL FROM this works. can change filename
+// http://stackoverflow.com/questions/32184589/renaming-an-uploaded-file-using-multer-doesnt-work-express-js
+
+// var options = multer.diskStorage({ destination : 'uploads/' ,
+//   filename: function (req, file, cb) {
+//   	console.log(file);
+//     cb(null, (Math.random().toString(36)+'00000000000000000').slice(2, 10) + Date.now() + path.extname(file.originalname));
+//   }
+// });
+
+// sama kayak nama asli file yg di upload
+var options = multer.diskStorage({ destination : 'public/img' ,
+  filename: function (req, file, cb) {
+  	console.log(file);
+    cb(null, file.originalname);
+  }
+});
+
+// var upload = multer({storage: storage});
+
+var upload= multer({ storage: options });
+
+
 
 var connection = mysql.createConnection({
 	host 	: 'localhost',
@@ -28,7 +62,11 @@ router.get('/new', function(req, res, next) {
 	res.render('new');
 })
 
-router.post('/new', multer({dest: './public/img/portfolio'}).single('projectimage'), function(req, res, next){
+
+
+router.post('/new', upload.single('projectimage'), function(req, res, next){
+
+// router.post('/new', upload.fields([{ name: 'file1', maxCount: 1 }, { name: 'file2', maxCount: 1 }]), function(req, res, next){	
 	// get form values
 	var title 		= req.body.title;
 	var description = req.body.description;
